f8e3b8d4a31df754e5ec32fc9a024f6ecb8b7748
Kleer LA <kleer@kleer-sandbox.(none)>
2014-12-05 14:33:22 -0500

    refactor completo

--- lib/SimonRecargado.rb
+++ lib/SimonRecargado.rb
@@ -4,33 +4,18 @@ class SimonRecargado
 	end
 
 	def generarLista(num)
-		imagenesRetorno = ""
 		secuenciaRetorno = ""
 		$i = 0
 		while $i < num.to_i do
-				imagen = @imagenes[rand(4)-1]
-				imagenesRetorno =  imagenesRetorno + "<img class='imagenSeq' src='" + getUrl(imagen) + "' />"
-				if(secuenciaRetorno.length == 0)
-					secuenciaRetorno = imagen
-				else
-					secuenciaRetorno = secuenciaRetorno + "," + imagen
-				end
+			imagen = @imagenes[rand(4)-1]
+		
+			if(secuenciaRetorno.length == 0)
+				secuenciaRetorno = imagen
+			else
+				secuenciaRetorno = secuenciaRetorno + "," + imagen
+			end
 			$i += 1	
 		end
-		imagenesRetorno + "&&" + secuenciaRetorno
-	end
-
-	def getUrl(llave)
-		img = ""
-		if(llave == 'circulo')
-			img = "http://www.arquba.com/curso-sketchup-gratis/entidades-de-circulo/index_ren-Circle.jpg"
-		elsif (llave == 'cuadrado')
-			img = "http://doc.qt.digia.com/4.6/images/used-in-examples/widgets/tooltips/images/square.png"
-		elsif (llave == 'triangulo')
-			img = "http://img3.wikia.nocookie.net/__cb20121214140518/uncyclopedia/images/8/88/Triangle%28shape%29.jpg"
-		else
-			img = "http://img2.wikia.nocookie.net/__cb20130620210048/inciclopedia/images/1/18/Estrella_amarilla.png"
-		end
-		img
+		secuenciaRetorno
 	end
 end
