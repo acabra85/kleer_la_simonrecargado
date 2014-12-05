01e3fff056647e22e7dde174ed81e37ef7c89339
Kleer LA <kleer@kleer-sandbox.(none)>
2014-12-05 14:55:35 -0500

    ajuste

--- lib/SimonRecargado.rb
+++ lib/SimonRecargado.rb
@@ -1,13 +1,13 @@
 class SimonRecargado
 	def initialize
-		@imagenes = ["estrella","triangulo","cuadrado", "circulo"]
+		@imagenes = ["estrella","triangulo","cuadrado", "circulo", "kleer"]
 	end
 
 	def generarLista(num)
 		secuenciaRetorno = ""
 		$i = 0
 		while $i < num.to_i do
-			imagen = @imagenes[rand(4)-1]
+			imagen = @imagenes[rand(@imagenes.length)-1]
 		
 			if(secuenciaRetorno.length == 0)
 				secuenciaRetorno = imagen
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,5 +13,18 @@ describe SimonRecargado do
 	it "Generar una lista de longitud 4 cuando envie el parametro 4" do
 		@simon.generarLista("4").split(",").length.should == 4
 	end
+
+
+	it "Generar una lista vacia cuando envie el parametro 0" do
+		@simon.generarLista("0").split(",").length.should == 0 
+	end
+
+	it "Generar una lista vacia cuando envie el parametro erroneo" do
+		@simon.generarLista("abc").split(",").length.should == 0 
+	end
+
+	it "Generar una lista vacia cuando no envie el parametro" do
+		@simon.generarLista("").split(",").length.should == 0 
+	end
 end
 
